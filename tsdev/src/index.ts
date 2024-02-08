b671410093141e3482d4a40770f34d4261fe1583
sadKor <91884195+sadKor@users.noreply.github.com>
2024-02-08 10:17:52 +0000

    added express and ports

--- tsdev/src/index.ts
+++ tsdev/src/index.ts
@@ -1,10 +1,35 @@
-class Main {
-    static sayHello(n:number) {
-        for (let i=0; i<n; i++) console.log("Hello World");
-    }
-}
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import { Queue } from "./Queue";
 
-Main.sayHello(3);
-Main.sayHello(4);
-console.log("Das funktioniert!");
-console.log("super!");
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 3000; 
+const bodyParser = require('body-parser');
+let queue = new Queue; 
+let value = "";
+
+ app.use(bodyParser.json());
+
+app.get("/", (req: Request, res: Response) => {
+
+    res.send(`Es sind momentan ${queue.length()} Elemente in der Queue`);
+  });
+
+app.post("/enqueue", (req: Request, res: Response) => {
+
+    var req1 = req.query;
+    res.send(queue.enqueue(req1.value));
+    res.send(`Elemenet der Queue hinzugefügt`);
+    res.status(200).json(queue);
+})
+
+app.get("/peek", (req: Request, res: Response) => {
+    res.send(`The first Element in the Queue is ${queue.peek()}`);
+})
+  
+  app.listen(port, () => {
+    console.log(`[server]: Server is running at http://localhost:${port}`);
+  });
